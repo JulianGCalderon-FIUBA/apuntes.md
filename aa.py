863e30b2014f7c49c343e0055537d9185d593478
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-08-17 00:20:39 -0300

    remove temp script

--- aa.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-vault = Path(".")
-
-for file in vault.rglob("*"):
-    for c in ["á", "é", "í", "ó", "ú", "Á", "É", "Í", "Ó", "Ú", "ñ", "Ñ"]:
-        if c in str(file):
-            print(file)
